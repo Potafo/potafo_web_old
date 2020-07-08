c77f624c6927abf997e2e7d1e5b33cd82d93877f
Habeeb <habeebmambeethi@gmail.com>
2020-07-08 03:27:43 -0400

    Earnings turned off

--- app/Http/Controllers/StaffReportController.php
+++ app/Http/Controllers/StaffReportController.php
@@ -53,7 +53,10 @@ class StaffReportController extends Controller
     
     public function filter_staff_reports(Request $request)
     {
-        return response('{}');
+        return response('{
+            "msg": "Exist",
+            "report_data": []
+        }');
         $timezone = 'ASIA/KOLKATA';
         $staffid   =   $request['staff_id'];
         $reports_name   =   $request['reports_name'];
